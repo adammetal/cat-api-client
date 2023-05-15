7f8343f378845085e455237f463722a17f14dd4a
adam.metal <adam.metal@codecool.com>
2023-05-15 10:00:38 +0200

    add the fetch function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+const URL = "https://api.thecatapi.com/v1/images/search?limit=";
+
+function fetchCats(limit = 10) {
+  return fetch(URL + limit)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      return data;
+    });
+}
