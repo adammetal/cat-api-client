31b64c1c6a8fa08b8d52b2562703570d8355b318
adam.metal <adam.metal@codecool.com>
2023-05-15 10:06:18 +0200

    refactor the fetch function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,13 @@
-const URL = "https://api.thecatapi.com/v1/images/search?limit=";
+const CAT_URL = "https://api.thecatapi.com/v1/images/search?limit=";
 
-function fetchCats(limit = 10) {
-  return fetch(URL + limit)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      return data;
-    });
+function get(url) {
+  return fetch(url).then(function (res) {
+    return res.json();
+  });
+}
+
+function getCatsFromApi(limit = 10) {
+  return get(URL + limit).then(function (data) {
+    return data;
+  });
 }
