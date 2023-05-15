de7e8a51402c0aaed7e67d310defdd42227d5543
adam.metal <adam.metal@codecool.com>
2023-05-15 10:03:45 +0200

    shows limit input fuield on the page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+const root = document.querySelector('#root');
+
+function renderLimitInputField() {
+  const limitEl = document.createElement('input');
+  limitEl.id = 'limit';
+  limitEl.type = 'number';
+  limitEl.placeholder = 'Limit...';
+  root.append(limitEl);
+}
+
+renderLimitInputField();
